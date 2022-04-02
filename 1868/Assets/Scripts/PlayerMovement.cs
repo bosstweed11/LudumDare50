e8d7904b6ea6afa69f531ca3c9ea209bd1a711ed
andy <acbeers1@gmail.com>
2022-04-01 23:13:15 -0400

    throwing potatoes

--- 1868/Assets/Scripts/PlayerMovement.cs
+++ 1868/Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
-using JetBrains.Rider.Unity.Editor;
+using System;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Debug = UnityEngine.Debug;
 
 public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] private float playerSpeed;
-
+    [SerializeField] private float throwSpeed;
+    [SerializeField] private GameObject tater;
     
     private Vector2 moveInput;
     private Rigidbody2D _rigidbody2D;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -31,5 +33,28 @@ public class PlayerMovement : MonoBehaviour
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+
+    void OnFire()
+    {
+        PrintVector(Mouse.current.position.ReadValue(), "original mouse position");
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        PrintVector(pos, "mouse position");
+        Vector3 trans = Camera.main.ScreenToWorldPoint(transform.position);
+        var throwVector = pos - transform.position;
+        var thisPotato= Instantiate(tater, transform.position, transform.rotation);
+        PrintVector(throwVector, "throw vector");
+        thisPotato.GetComponent<Rigidbody2D>().velocity = new Vector2
+        {
+            x = throwVector.x * throwSpeed,
+            y = throwVector.y * throwSpeed
+        };
+    }
+
+    void PrintVector(Vector3 vector, string label)
+    {
+        Debug.Log(label);
+        Debug.Log("x : " + vector.x);
+        Debug.Log("y : " + vector.y);
     }
 }
---
+++ 1868/Assets/Scripts/PotatoLauncher.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotatoLauncher : MonoBehaviour
+{
+    [SerializeField] private GameObject tater;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
