a02ffa5e42b361a5717cdbe82074593d7e86fc3a
andy <acbeers1@gmail.com>
2022-04-01 23:44:52 -0400

    voting in many ballot boxes, counting votes in each box

--- 1868/Assets/Scripts/BallotBoxController.cs
+++ 1868/Assets/Scripts/BallotBoxController.cs
@@ -1,11 +1,14 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BallotBoxController : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI democraticVoteUI;
+    [SerializeField] private TextMeshProUGUI republicanVoteUI;
     // Start is called before the first frame update
+    private int democraticVoteCount = 0;
+    private int republicanVoteCount = 0;
+    private float counter = 0;
     void Start()
     {
         
@@ -14,16 +17,32 @@ public class BallotBoxController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        counter += Time.deltaTime;
+        if (counter > 1)
+        {
+            republicanVoteCount++;
+            UpdateRepublicanText();
+            counter = 0;
+        }
     }
-
-
+    
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag.Equals("Potato"))
         {
-            Debug.Log("yum");
             Destroy(col.gameObject);
+            democraticVoteCount++;
+            UpdateDemocratText();
         }
+    }
+
+    private void UpdateRepublicanText()
+    {
+        republicanVoteUI.text = republicanVoteCount.ToString();
+    }
+    
+    private void UpdateDemocratText()
+    {
+        democraticVoteUI.text = democraticVoteCount.ToString();
     }
 }
